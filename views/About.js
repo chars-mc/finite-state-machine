9e0819b5834bd39389614f434fa54de6c3289d58
Carlos Manuel <chars.dev@gmail.com>
2020-11-28 15:06:34 -0600

    add views

---
+++ views/About.js
@@ -0,0 +1,7 @@
+const About = Vue.component("about", {
+  template: `
+    <div>
+      <h3>About page</h3>
+    </div>
+  `
+});
---
+++ views/MainContent.js
@@ -0,0 +1,7 @@
+const MainContent = Vue.component("main-content", {
+  template: `
+    <div>
+      <h3>Main content</h3>
+    </div>
+  `
+});
